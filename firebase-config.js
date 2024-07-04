27c6152c35cdf569385bc1125d3a4aa30dc1b05a
Suzana Mota <45774928+svmteste@users.noreply.github.com>
2024-07-04 14:36:09 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,4 @@
 // Configuração do Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyBhycJZ7wHsFYGDcdoZRazutKVNfEKa2SQ",
-    authDomain: "ifnmg-esporte.firebaseapp.com",
-    projectId: "ifnmg-esporte",
-    storageBucket: "ifnmg-esporte.appspot.com",
-    messagingSenderId: "844194342463",
-    appId: "1:844194342463:web:67655c8443b7a3c7e23a8a",
-    measurementId: "G-CTVQNN48YC"
-  };
-
 // const firebaseConfig = {
 //     apiKey: "SuaApiKey",
 //     authDomain: "SeuDominio.firebaseapp.com",
